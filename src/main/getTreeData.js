22a30c5db7b40fed2c382f4087f97844f5a62f95
UT LEE <dtmaster@empal.com>
2020-04-13 16:30:10 +0900

    little changes

--- src/main/getTreeData.js
+++ src/main/getTreeData.js
@@ -2,7 +2,9 @@ const getTreeData = require("./directoryTree.js");
 
 const directoryTreeData = (path) => {
   //let directoryTreeData = getTreeData("C:/GPT");
-  let data = getTreeData(path);
+  let data = getTreeData(path, {
+    extensions: /\.(bmp|jpg|jpeg|jpe|gif|tif|tiff|png)$/,
+  });
   console.log(directoryTreeData);
   return data;
 };
